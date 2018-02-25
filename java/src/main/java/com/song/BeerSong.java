7dd4a021852c839ad3079c049af3b164975e43da
Rachel <raquel.moreno.carmena@gmail.com>
2018-02-25 21:20:55 +0100

    Refactor: considering any number of bottles

--- java/src/main/java/com/song/BeerSong.java
+++ java/src/main/java/com/song/BeerSong.java
@@ -12,11 +12,10 @@ public class BeerSong {
         if (bottlesNumber == 2)
             return "2 bottles of beer on the wall, 2 bottles of beer.\n" +
                     "Take one down and pass it around, 1 bottle of beer on the wall.\n\n";
-        if (bottlesNumber == 3)
-            return "3 bottles of beer on the wall, 3 bottles of beer.\n" +
-                    "Take one down and pass it around, 2 bottles of beer on the wall.\n\n";
-        return "99 bottles of beer on the wall, 99 bottles of beer.\n" +
-                "Take one down and pass it around, 98 bottles of beer on the wall.\n\n";
+        return String.format(
+                "%d bottles of beer on the wall, %d bottles of beer.\n" +
+                        "Take one down and pass it around, %d bottles of beer on the wall.\n\n",
+                bottlesNumber, bottlesNumber, bottlesNumber - 1);
     }
 
     public String singSong() {
