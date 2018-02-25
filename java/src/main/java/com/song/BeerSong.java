6c5e5084b3d94eb7fb044d0b4321092cf8448b64
Rachel <raquel.moreno.carmena@gmail.com>
2018-02-25 21:13:13 +0100

    Green: sing first verse

--- java/src/main/java/com/song/BeerSong.java
+++ java/src/main/java/com/song/BeerSong.java
@@ -3,7 +3,8 @@ package com.song;
 public class BeerSong {
 
     public String sing(int bottlesNumber, int versesNumber) {
-        return null;
+        return "99 bottles of beer on the wall, 99 bottles of beer.\n" +
+                "Take one down and pass it around, 98 bottles of beer on the wall.\n\n";
     }
 
     public String singSong() {
