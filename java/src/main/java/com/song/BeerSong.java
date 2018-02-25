ea880ba577dd46c64a6ce578ea8f0f4375de4f1b
Rachel <raquel.moreno.carmena@gmail.com>
2018-02-25 21:16:01 +0100

    Green: verse with 1 bottle

--- java/src/main/java/com/song/BeerSong.java
+++ java/src/main/java/com/song/BeerSong.java
@@ -3,6 +3,9 @@ package com.song;
 public class BeerSong {
 
     public String sing(int bottlesNumber, int versesNumber) {
+        if (bottlesNumber == 1)
+            return "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+                    "Take it down and pass it around, no more bottles of beer on the wall.\n\n";
         if (bottlesNumber == 2)
             return "2 bottles of beer on the wall, 2 bottles of beer.\n" +
                     "Take one down and pass it around, 1 bottle of beer on the wall.\n\n";
