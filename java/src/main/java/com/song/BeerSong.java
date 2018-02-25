dcb24ca7100101843b0df5f930f8b1131589643d
Rachel <raquel.moreno.carmena@gmail.com>
2018-02-25 21:14:15 +0100

    Green: sing last generic verse

--- java/src/main/java/com/song/BeerSong.java
+++ java/src/main/java/com/song/BeerSong.java
@@ -3,6 +3,9 @@ package com.song;
 public class BeerSong {
 
     public String sing(int bottlesNumber, int versesNumber) {
+        if (bottlesNumber == 3)
+            return "3 bottles of beer on the wall, 3 bottles of beer.\n" +
+                    "Take one down and pass it around, 2 bottles of beer on the wall.\n\n";
         return "99 bottles of beer on the wall, 99 bottles of beer.\n" +
                 "Take one down and pass it around, 98 bottles of beer on the wall.\n\n";
     }
