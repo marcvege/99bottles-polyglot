9bacff9580efd7bf29d422acea3fa25524049af1
Marc Villagrasa <marcvege@gmail.com>
2018-03-02 19:12:16 +0100

    Generate first vers

--- java/src/main/java/com/song/BeerSong.java
+++ java/src/main/java/com/song/BeerSong.java
@@ -2,11 +2,13 @@ package com.song;
 
 public class BeerSong {
 
-    public String sing(int bottlesNumber, int versesNumber) {
-        return null;
-    }
-
     public String singSong() {
         return null;
     }
+
+    public String sing(int bottlesNumber, int versesNumber) {
+        return "99 bottles of beer on the wall, 99 bottles of beer.\n" +
+                "Take one down and pass it around, 98 bottles of beer on the wall.\n\n";
+    }
+
 }
