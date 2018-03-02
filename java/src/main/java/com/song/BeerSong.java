5ebdae136760a2a455a1a5827f56d7287f209e2d
Marc Villagrasa <marcvege@gmail.com>
2018-03-02 19:19:08 +0100

    Refactor join cases

--- java/src/main/java/com/song/BeerSong.java
+++ java/src/main/java/com/song/BeerSong.java
@@ -7,14 +7,14 @@ public class BeerSong {
     }
 
     public String sing(int bottlesNumber, int versesNumber) {
-        if (bottlesNumber == 99)
-            return "99 bottles of beer on the wall, 99 bottles of beer.\n" +
-                    "Take one down and pass it around, 98 bottles of beer on the wall.\n\n";
-        if (bottlesNumber == 3)
-        return "3 bottles of beer on the wall, 3 bottles of beer.\n" +
-                "Take one down and pass it around, 2 bottles of beer on the wall.\n\n";
-        return "2 bottles of beer on the wall, 2 bottles of beer.\n" +
-                "Take one down and pass it around, 1 bottle of beer on the wall.\n\n";
+        if (bottlesNumber == 2)
+            return "2 bottles of beer on the wall, 2 bottles of beer.\n" +
+                    "Take one down and pass it around, 1 bottle of beer on the wall.\n\n";
+        
+        return String.format("%s bottles of beer on the wall, %s bottles of beer.\n" +
+                        "Take one down and pass it around, %s bottles of beer on the wall.\n\n",
+                bottlesNumber, bottlesNumber, bottlesNumber - 1);
+
 
     }
 
